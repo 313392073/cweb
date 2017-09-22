63ed9505c295558113ae84e0f0cfa2cc894f56d8
qijing <313392073@qq.com>
2017-09-22 17:20:20 +0800

    '增加轮播'

--- public/js/common.js
+++ public/js/common.js
@@ -2,9 +2,6 @@
  * Created by Administrator on 2017/9/21.
  */
 $(function () {
-    $("#navList>ul li").not("li.ltext").hover(function () {
-        $(this).addClass("act").siblings("li").not("li.ltext").removeClass("act");
-    })
     $("#navList>ul li").not("li.ltext").on("click", function () {
         $(this).addClass("act").siblings("li").not("li.ltext").removeClass("act");
     })
